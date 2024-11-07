b157efff28116c3b9b55fb66952fe98750fdb38c
@RupertoTacuche <murcio2490@gmail.com>
2024-11-07 08:51:02 -0600

    ProductCard primera parte funcionando

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,4 +1,4 @@
-import {Box, Button, Container, Heading, Input, useColorModeValue, VStack, } from '@chakra-ui/react'
+import {Box, Button, Container, Heading, Input, useColorModeValue, useToast, VStack, } from '@chakra-ui/react'
 import {useState} from 'react'
 import { useProductStore } from '../store/product'
 
@@ -9,14 +9,29 @@ const CreatePage = () => {
     image: "",
   })
 
+  const toast = useToast()
   const { createProduct } = useProductStore()
 
   //console.log(createProduct)
 
   const handleAddProduct = async () =>{
     const {success, message} = await createProduct(newProduct)
-    console.log("Succes:", success)
-    console.log("Message:", message)
+    if(!success) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        isClosable: true
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "error",
+        isClosable: true
+      });
+    }
+    setNewProduct({name: "", price: "", image: ""});
   }
 
   return (
